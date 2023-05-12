6355d17d9df0161b6ec52162c3f71898b8f7e206
j.rainey <jonathanmichael5150@gmail.com>
2023-05-12 06:11:29 -0500

    Create WeeklyChallenge4.js

---
+++ WeeklyChallenge4.js
@@ -0,0 +1,35 @@
+function LongestWord(sen) { 
+
+  // code goes here  
+  stringArray = sen.split("");
+  var longestString = "";
+  var tempString = "";
+  var longestArray = [];
+  let i = 0;
+
+  while ( i < stringArray.length ) {
+
+    var regExp = /[a-zA-Z]/;
+           
+    if (regExp.test(stringArray[i])) {
+      longestArray.push(stringArray[i]);
+      if ( longestArray.join("").length > tempString.length && (i >= 1)) {
+        var tempString = longestArray.join("");
+      }
+    } else {
+      if ( longestArray.join("").length > tempString.length ) {
+        var tempString = longestArray.join("");
+      }
+      var longestArray = [];
+    }
+
+    i++;
+
+  }
+
+  return tempString; 
+
+}
+   
+// keep this function call here 
+console.log(LongestWord(readline()));
